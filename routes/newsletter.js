18c0605eb3550320d575af3be60c303cfad06bd5
atuli-gorgodze <atuligorgodze27@gmail.com>
2025-06-11 12:44:26 +0400

    final project

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -1,29 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const Blog = require('../models/blog'); // make sure this path is correct
+const Blog = require('../models/blog');
+const Newsletter = require('../models/newsletter');
 
-router.get('/', async function (req, res) {
-    if (!req.session.user) {
-        return res.redirect('/login');
-    }
+router.get('/', async function (req, res, next) {
+    const email = req.session.user ? req.session.user.email : null;
+    const blogs = await Blog.find({});
+    blogs.reverse()
 
-    try {
-        const blogs = await Blog.find().sort({ createdAt: -1 }); // get blogs from DB
-        res.render('newsletter', {
-            title: 'Newsletter',
-            email: req.session.user.email,
-            blogs: blogs
-        });
-    } catch (err) {
-        console.error('Error fetching blogs:', err);
-        res.status(500).send('Internal Server Error');
-    }
+    res.render('newsletter', {error: null, email, blogs});
 });
 
-router.post('/', function (req, res) {
-    const email = req.body.email;
-    console.log('Subscribed email:', email);
-    res.send('Thanks for subscribing!');
+router.post('/', async function (req, res, next) {
+    const { email } = req.body
+
+    try {
+        const subscriberData = {
+            id: String(Date.now()),
+            author: email,
+        }
+
+        const newSubscriber = new Newsletter(subscriberData);
+        const result = await newSubscriber.save();
+        console.log(result);
+    } catch (err) {
+        console.log(err);
+    }
+
+    res.redirect('/newsletter');
 });
 
 module.exports = router;
